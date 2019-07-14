7b3779d1bef881eac523d1165e8c55748179eef3
kdhertog <koendenhertog@icloud.com>
2019-07-14 21:41:09 +0200

    Version 0.1.1

--- loadboard.py
+++ loadboard.py
@@ -1,9 +1,4 @@
-#####################
-# Engine for the game of life
-# Written by Koen den Hertog
-
-import numpy as np
-import os
+# OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD
 
 def load_board(filename): #Loads an existing board from a .lif file. Only works for the 'Life 1.05' format
     folder = os.path.dirname(__file__)
@@ -102,24 +97,3 @@ def load_board(filename): #Loads an existing board from a .lif file. Only works 
         board[xpos_initial:xpos_initial+len(initial[:,0]),ypos_initial:ypos_initial+len(initial[0,:])] = initial  # Load initial into the board
 
     return board
-
-def engine(board):
-    # Create new board for calculations
-    nx = len(board[0,:])
-    ny = len(board[:,0])
-    calc_board = np.zeros((nx+2,ny+2)) 
-    calc_board[1:nx+1,1:ny+1] = board
-    
-    # Shift the board in each of the eight directions, and sum them
-    sum_board = calc_board[0:nx+0,0:ny+0]+ \
-                calc_board[0:nx+0,1:ny+1]+ \
-                calc_board[0:nx+0,2:ny+2]+ \
-                calc_board[1:nx+1,0:ny+0]+ \
-                calc_board[1:nx+1,2:ny+2]+ \
-                calc_board[2:nx+2,0:ny+0]+ \
-                calc_board[2:nx+2,1:ny+1]+ \
-                calc_board[2:nx+2,2:ny+2]
-
-    # Return the new board
-    new_board = np.where(sum_board == 2,board,(np.where(sum_board == 3,1,0)))
-    return new_board
